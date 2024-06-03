cb1ae6b7a97917a920a6eb4246a82e869c0a871d
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2024-06-03 21:30:43 +0200

    created and working  on Express Functionality section and Cards section

--- src/App.jsx
+++ src/App.jsx
@@ -142,9 +142,9 @@ function App() {
                 Pariatur placeat est rerum maiores consequuntur modi voluptatem
                 molestiae tenetur ipsa neque.
               </p>
-              <button className="block w-[210px] h-[52px] bg-white rounded-full">
+              <button className="block w-[210px] h-[52px] bg-white rounded-full hover:bg-gradient-to-r from-purple-500 to-pink-500 ">
                 <a
-                  className="block uppercase text-gray-700 font-normal text-base"
+                  className="block uppercase text-gray-700 hover:text-white font-normal text-base"
                   href="#"
                 >
                   Start free trail
@@ -205,6 +205,119 @@ function App() {
         </div>
       </section>
       {/* END Hero Section */}
+      {/* Premium Features */}
+      <section className="w-full h-fit">
+        <div className="container mx-auto px-4 xl:max-w-[1280px]">
+          <div className="flex justify-start items-center my-10 px-4 py-2 rounded-full w-52 shadow-xl">
+            <div>
+              <svg
+                class="svg-inline--fa fa-lightbulb fa-w-11 text-primary mr-1"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="far"
+                data-icon="lightbulb"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 352 512"
+                data-fa-i2svg=""
+                width="16px"
+                color="#6d36e1"
+              >
+                <path
+                  fill="currentColor"
+                  d="M176 80c-52.94 0-96 43.06-96 96 0 8.84 7.16 16 16 16s16-7.16 16-16c0-35.3 28.72-64 64-64 8.84 0 16-7.16 16-16s-7.16-16-16-16zM96.06 459.17c0 3.15.93 6.22 2.68 8.84l24.51 36.84c2.97 4.46 7.97 7.14 13.32 7.14h78.85c5.36 0 10.36-2.68 13.32-7.14l24.51-36.84c1.74-2.62 2.67-5.7 2.68-8.84l.05-43.18H96.02l.04 43.18zM176 0C73.72 0 0 82.97 0 176c0 44.37 16.45 84.85 43.56 115.78 16.64 18.99 42.74 58.8 52.42 92.16v.06h48v-.12c-.01-4.77-.72-9.51-2.15-14.07-5.59-17.81-22.82-64.77-62.17-109.67-20.54-23.43-31.52-53.15-31.61-84.14-.2-73.64 59.67-128 127.95-128 70.58 0 128 57.42 128 128 0 30.97-11.24 60.85-31.65 84.14-39.11 44.61-56.42 91.47-62.1 109.46a47.507 47.507 0 0 0-2.22 14.3v.1h48v-.05c9.68-33.37 35.78-73.18 52.42-92.16C335.55 260.85 352 220.37 352 176 352 78.8 273.2 0 176 0z"
+                ></path>
+              </svg>
+            </div>
+            <div className="text-black text-base leading-6 font-medium">
+              <span className="mr-1 text-purple-800">Premium</span>Features
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* END Premium Features */}
+      {/* Express Functionality section */}
+      <section className="w-full h-fit mb-10">
+        <div className="container mx-auto px-4 xl:max-w-[1280px]">
+          <h2 className="text-[32px] leading-[48px] font-semibold text-black md:text-4xl lg:text-5xl lg:leading-[62px]">
+            Express Functionality
+          </h2>
+          <span className="block w-10 border border-purple-700 my-3"></span>
+          <p className="text-base leading-6 font-normal text-gray-400 md:w-[500px] ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
+            aperiam eligendi ut voluptate ipsum amet sapiente tempore eos,
+            officia consequatur.
+          </p>
+        </div>
+      </section>
+      {/* END Express Functionality section */}
+      {/* Cards */}
+      <section className="w-full h-fit mb-10">
+        <div className="container mx-auto px-4 md:grid md:gap-4 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 xl:max-w-[1280px]">
+          <div className="flex flex-col justify-center items-center shadow-xl py-5 px-4 h-fit rounded-2xl w-full sm:max-w-[600px] mx-auto md:h-[280px]">
+            <div className="my-2">icon</div>
+            <h3 className="text-2xl leading-9 text-black font-semibold text-center mb-3">
+              Fully functional
+            </h3>
+            <p className="text-center text-base leading-6 font-normal text-gray-500 mb-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              eligendi?
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center shadow-xl py-5 px-4 h-fit rounded-2xl w-full sm:max-w-[600px] mx-auto md:h-[280px]">
+            <div className="my-2">icon</div>
+            <h3 className="text-2xl leading-9 text-black font-semibold text-center mb-3">
+              Fully functional
+            </h3>
+            <p className="text-center text-base leading-6 font-normal text-gray-500 mb-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              eligendi?
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center shadow-xl py-5 px-4 h-fit rounded-2xl w-full sm:max-w-[600px] mx-auto md:h-[280px]">
+            <div className="my-2">icon</div>
+            <h3 className="text-2xl leading-9 text-black font-semibold text-center mb-3">
+              Fully functional
+            </h3>
+            <p className="text-center text-base leading-6 font-normal text-gray-500 mb-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              eligendi?
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center shadow-xl py-5 px-4 h-fit rounded-2xl w-full sm:max-w-[600px] mx-auto md:h-[280px]">
+            <div className="my-2">icon</div>
+            <h3 className="text-2xl leading-9 text-black font-semibold text-center mb-3">
+              Fully functional
+            </h3>
+            <p className="text-center text-base leading-6 font-normal text-gray-500 mb-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              eligendi?
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center shadow-xl py-5 px-4 h-fit rounded-2xl w-full sm:max-w-[600px] mx-auto md:h-[280px]">
+            <div className="my-2">icon</div>
+            <h3 className="text-2xl leading-9 text-black font-semibold text-center mb-3">
+              Fully functional
+            </h3>
+            <p className="text-center text-base leading-6 font-normal text-gray-500 mb-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              eligendi?
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center shadow-xl py-5 px-4 h-fit rounded-2xl w-full sm:max-w-[600px] mx-auto md:h-[280px]">
+            <div className="my-2">icon</div>
+            <h3 className="text-2xl leading-9 text-black font-semibold text-center mb-3">
+              Fully functional
+            </h3>
+            <p className="text-center text-base leading-6 font-normal text-gray-500 mb-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              eligendi?
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* END Cards */}
+      {/*  */}
     </>
   );
 }
